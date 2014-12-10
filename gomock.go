0983a52d99f99e8156b789db38e49a6eb5d53497
Hirotaka Osaki <osaki_hirotaka@cyberagent.co.jp>
2014-12-11 08:55:03 +0900

    Changed interface

--- gomock.go
+++ gomock.go
@@ -1,32 +1,71 @@
 // gomock package is mocking http request server.
 package gomock
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"regexp"
+)
 
-type Layer func(req *http.Request) (resp *http.Response, err error)
+type Handle func(req *http.Request) *http.Response
+
+type Layer struct {
+	Pattern    *regexp.Regexp
+	HandleFunc Handle
+}
 
 type Transport struct {
 	Transport http.RoundTripper
-	Layers    []Layer
+	layers    []Layer
 }
 
 func NewTransport() *Transport {
 	return &Transport{
 		Transport: http.DefaultTransport,
-		Layers:    []Layer{},
+		layers:    []Layer{},
 	}
+}
+
+func (t *Transport) Stub(m interface{}, handle Handle) error {
+	l, err := newLayer(m, handle)
+	if err != nil {
+		return err
+	}
+	t.layers = append(DefaultTransport.layers, l)
+	return nil
 }
 
 var DefaultTransport = NewTransport()
 
-func (t *Transport) AddLayer(handler Layer) {
-	t.Layers = append(t.Layers, handler)
+func Stub(m interface{}, handle Handle) error {
+	l, err := newLayer(m, handle)
+	if err != nil {
+		return err
+	}
+	DefaultTransport.layers = append(DefaultTransport.layers, l)
+	return nil
+}
+
+func newLayer(m interface{}, handle Handle) (Layer, error) {
+	switch v := m.(type) {
+	case *regexp.Regexp:
+		return Layer{
+			Pattern:    v,
+			HandleFunc: handle,
+		}, nil
+	case string:
+		return Layer{
+			Pattern:    regexp.MustCompile(v),
+			HandleFunc: handle,
+		}, nil
+	}
+	return Layer{}, fmt.Errorf("invalid m %v", m)
 }
 
 func (t *Transport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
-	for _, l := range t.Layers {
-		resp, err := l(req)
-		if resp != nil || err != nil {
+	for _, l := range t.layers {
+		resp := l.HandleFunc(req)
+		if resp != nil {
 			return resp, err
 		}
 	}
--- gomock_test.go
+++ gomock_test.go
@@ -6,6 +6,20 @@ import (
 	"testing"
 )
 
+func TestStub(t *testing.T) {
+	Stub("/", func(req *http.Request) *http.Response {
+		r := &http.Response{
+			Status:     "200 OK",
+			StatusCode: http.StatusOK,
+		}
+		return r
+	})
+
+	if v := len(DefaultTransport.layers); v != 1 {
+		t.Errorf("len(DefaultTransport) == 1, but %v", v)
+	}
+}
+
 func TestRoundTrip(t *testing.T) {
 	handler := func(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
@@ -14,12 +28,12 @@ func TestRoundTrip(t *testing.T) {
 	defer server.Close()
 
 	transport := NewTransport()
-	transport.AddLayer(func(req *http.Request) (resp *http.Response, err error) {
+	transport.Stub("/", func(req *http.Request) *http.Response {
 		r := &http.Response{
 			Status:     "200 OK",
 			StatusCode: http.StatusOK,
 		}
-		return r, nil
+		return r
 	})
 
 	client := &http.Client{
